48122fec3a7b4bfc055bcdfe6b7b0e1b30540579
Emmanuel Simon Pierre Branlard <ebra@hpc-fe1.(none)>
2013-11-02 19:11:42 +0100

    Added vim setup

--- dotfiles/vim/SetupVim.sh
+++ dotfiles/vim/SetupVim.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+mkdir -p bundle
 cd bundle
 
 git clone https://github.com/kien/ctrlp.vim/
 git clone https://github.com/elmanuelito/vim-simple-compile
 git clone https://github.com/elmanuelito/vim-simple-comment
-
-
+git clone https://github.com/kien/rainbow_parentheses.vim
+git clone https://github.com/vim-scripts/Align
 if [ $HOSTNAME == 'work' ]
 then
     echo "Work"
-# rainbow_parentheses.vim/
 # syntastic/
 # tlib_vim/
 # vim-addon-mw-utils/
 # vim-airline/
-# vim-align/
 # vim-easymotion/
 # vim-fugitive/
 # vim-markdown-folding/
