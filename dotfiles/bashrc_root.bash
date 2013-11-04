04b205317208e792d85713af0dc76905205d8933
elmanuelito <elmanuelito.github@gmail.com>
2013-11-04 07:25:20 +0100

    Bashrc root

--- dotfiles/bashrc_root.bash
+++ dotfiles/bashrc_root.bash
@@ -1,6 +1,6 @@
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export HOMEMANU=/home/manu
 export PATH=$PATH:$HOMEMANU/Config/scripts/
-
 # source $HOME2"Config/dotfiles/bashrc_machine.bash"
 source $HOMEMANU/"Config/dotfiles/bashrc_common.bash"
 # source $HOME2"Config/bash/git-completion.bash"
