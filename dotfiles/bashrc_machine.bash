d35900c34ba619a1ddaa16e5d2fb9cc14c70f0fe
elmanuelito <elmanuelito.github@gmail.com>
2013-11-20 17:10:33 +0100

    More env var

--- dotfiles/bashrc_machine.bash
+++ dotfiles/bashrc_machine.bash
@@ -14,6 +14,7 @@ then
     export OMNIVOR_MKF_DIR='/work/lib/OmniVor_lib/fortran/_includes/'
     export OMNIVOR_LIB_DIR='/work/lib/OmniVor_lib/fortran/_lib/linux-ia32/'
     export OMNIVOR_OBJ_DIR='/work/lib/OmniVor_lib/fortran/_build/linux-ia32/'
+    export OMNIVOR_BIN_DIR='/work/lib/OmniVor_lib/fortran/_bin/linux-ia32/'
     export OMNIVOR_SRC_DIR='/work/lib/OmniVor_lib/fortran/'
 
 
@@ -24,21 +25,42 @@ then
 
 elif [ $HOSTNAME == 'g-000.risoe.dk' ]
 then
+    # GORM
     alias ls='ls -F --color=always '
+    export OMNIVOR_MKF_DIR='/home/ebra/omnivor/_includes/'
+    export OMNIVOR_LIB_DIR='/home/ebra/omnivor/_lib/linux-ia32/'
+    export OMNIVOR_OBJ_DIR='/home/ebra/omnivor/_build/linux-ia32/'
+    export OMNIVOR_BIN_DIR='/home/ebra/omnivor/_bin/linux-ia32/'
+    export OMNIVOR_SRC_DIR='/home/ebra/omnivor/'
 elif [ $HOSTNAME == 'hpc-fe1' ]
 then
+    # hpc root
     alias gonodeX='qrsh -q mic_interactive-X'
     alias gonode='qrsh -q mic_interactive '
+    export OMNIVOR_MKF_DIR='/home/ebra/omnivor/_includes/'
+    export OMNIVOR_LIB_DIR='/home/ebra/omnivor/_lib/linux-ia32/'
+    export OMNIVOR_OBJ_DIR='/home/ebra/omnivor/_build/linux-ia32/'
+    export OMNIVOR_BIN_DIR='/home/ebra/omnivor/_bin/linux-ia32/'
+    export OMNIVOR_SRC_DIR='/home/ebra/omnivor/'
 
 elif [ $HOSTNAME == 'n-62-28-19' ]
 then
+    # hpc node
     source /opt/intel/2013.0.028/bin/ifortvars.sh intel64
     alias vtune='source /opt/intel/2013.0.028/vtune_amplifier_xe_2013/amplxe-vars.sh'
-
+    export OMNIVOR_MKF_DIR='/home/ebra/omnivor/_includes/'
+    export OMNIVOR_LIB_DIR='/home/ebra/omnivor/_lib/linux-ia32/'
+    export OMNIVOR_OBJ_DIR='/home/ebra/omnivor/_build/linux-ia32/'
+    export OMNIVOR_BIN_DIR='/home/ebra/omnivor/_bin/linux-ia32/'
+    export OMNIVOR_SRC_DIR='/home/ebra/omnivor/'
 elif [ $HOSTNAME == 'gray1' ]
 then
     alias ls='ls -F  '
-
+    export OMNIVOR_MKF_DIR='/home/ebra/omnivor/_includes/'
+    export OMNIVOR_LIB_DIR='/home/ebra/omnivor/_lib/linux-ia32/'
+    export OMNIVOR_OBJ_DIR='/home/ebra/omnivor/_build/linux-ia32/'
+    export OMNIVOR_BIN_DIR='/home/ebra/omnivor/_bin/linux-ia32/'
+    export OMNIVOR_SRC_DIR='/home/ebra/omnivor/'
 
 else
     echo "No bashrc specific commands for this hosts."
