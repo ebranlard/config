7d84baa3b4f85f0053e3c4ed591686518b843337
Emmanuel Branlard <ebra@dtu.dk>
2014-11-14 11:44:12 +0100

    _update from jess

--- path/PythonPath/ebra/__init__.py
+++ path/PythonPath/ebra/__init__.py
@@ -1,4 +1,8 @@
-# from export import*
-# from matlab import*
-# from plot import*
-# from require import*
+try:
+    from export import*
+    from matlab import*
+except Exception,e:
+    pass
+    
+from plot import*
+from require import*
