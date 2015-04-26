c02538ad1735f76444d83c3b526b9913d6939cee
elmanuelito <elmanuelito.github@gmail.com>
2015-04-26 14:15:25 +0200

    _update from home

---
+++ mime/Install-NotRoot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+xdg-mime install mkfile-mime.xml
+xdg-mime install batfile-mime.xml
+xdg-mime install readme-mime.xml
+xdg-mime install todo-mime.xml
+xdg-mime install fortran-mime.xml
+
+
+# xdg-icon-resource ins-mimetall --context mimetypes --size 48 myicon-file-type.png x-application-mytype
