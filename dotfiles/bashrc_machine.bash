411bcef69649de7b7ef3280d58ecfe280c5fc27e
Emmanuel Branlard <elmanuelito.github@gmail.com>
2015-12-07 16:20:12 +0100

    _update of profiles with unix file format

--- dotfiles/bashrc_machine.bash
+++ dotfiles/bashrc_machine.bash
@@ -5,12 +5,37 @@ export PYTHONPATH="$PYTHONPATH:$HOME/Config/path/PythonPath"
 alias ls='ls -F --color=always --group-directories-first'
 # alias grep='grep --color=always'
 alias grep='grep --color=auto'
-export PS1='\[[1m[33m\]\u\[[32m\]@\[[33m\]\h:\[[32m\]\w \[[33m\]\$ \[[32m\]\[[m\]'
+
+# --------------------------------------------------------------------------------
+# --- Colors 
+# --------------------------------------------------------------------------------
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+export PS1="$BYellow\u$BGreen@$BYellow\h:$BGreen\w $BYellow\$ $Color_Off"
 
 if [ $HOSTNAME == 'work' ]
 then
-
-    export PS1='\[[1m[33m\]\u\[[32m\]@\[[33m\]\h:\[[32m\]\w `whichBranch`\[[33m\]\$ \[[32m\]\[[m\]'
 
     source /opt/intel/bin/compilervars.sh intel64
     alias vtune='source /opt/intel/vtune_amplifier_xe_2013/amplxe-vars.sh'
@@ -18,9 +43,6 @@ then
     export PATH=$PATH:/work/lib/OmniVor/_src/_bin/linux-amd64
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
     
-#     export PATH=/home/manu/Work/cuda/bin:$PATH
-#     export LD_LIBRARY_PATH=/home/manu/Work/cuda/lib64:$LD_LIBRARY_PATH
-
     export OMNIVOR_MKF_DIR='/work/libs/OmniVor/_src/_mkf/'
     export OMNIVOR_INC_DIR='/work/libs/OmniVor/_src/_includes/'
     export OMNIVOR_LIB_DIR='/work/libs/OmniVor/_src/_lib/linux-amd64/'
@@ -31,21 +53,21 @@ then
 elif [ $HOSTNAME == 'olympe' ]
 then
 
-    source /opt/intel/bin/compilervars.sh intel64
-    export PATH=$PATH:/home/manu/Work-tmp/omnivor/_src/_bin/linux-amd64
-    export PS1='\[[1m[33m\]\u\[[32m\]@\[[33m\]\h:\[[32m\]\w `whichBranch`\[[33m\]\$ \[[32m\]\[[m\]'
-#    source /opt/intel/bin/compilervars.sh intel64
-#    alias vtune='source /opt/intel/vtune_amplifier_xe_2013/amplxe-vars.sh'
+    UNAME=`uname -a | awk -F _ '{print $1}'`
+    if [  $UNAME == 'CYGWIN' ] 
+    then
+        echo "-"$UNAME"-"
+    else
+        source /opt/intel/bin/compilervars.sh intel64
+        export PATH=$PATH:/home/manu/Work-tmp/omnivor/_src/_bin/linux-amd64
 
-#    export PATH=$PATH:/work/lib/OmniVor_lib/fortran/_bin/linux-amd64
-#    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-    
-    export OMNIVOR_MKF_DIR='/work/libs/OmniVor/_src/_mkf/'
-    export OMNIVOR_INC_DIR='/work/libs/OmniVor/_src/_includes/'
-    export OMNIVOR_LIB_DIR='/work/libs/OmniVor/_src/_lib/linux-amd64/'
-    export OMNIVOR_OBJ_DIR='/work/libs/OmniVor/_src/_build/linux-amd64/'
-    export OMNIVOR_BIN_DIR='/work/libs/OmniVor/_src/_bin/linux-amd64/'
-    export OMNIVOR_SRC_DIR='/work/libs/OmniVor/_src/'
+        export OMNIVOR_MKF_DIR='/work/libs/OmniVor/_src/_mkf/'
+        export OMNIVOR_INC_DIR='/work/libs/OmniVor/_src/_includes/'
+        export OMNIVOR_LIB_DIR='/work/libs/OmniVor/_src/_lib/linux-amd64/'
+        export OMNIVOR_OBJ_DIR='/work/libs/OmniVor/_src/_build/linux-amd64/'
+        export OMNIVOR_BIN_DIR='/work/libs/OmniVor/_src/_bin/linux-amd64/'
+        export OMNIVOR_SRC_DIR='/work/libs/OmniVor/_src/'HS
+    fi
 
 elif [ $HOSTNAME == 'g-000.risoe.dk' ]
 then
@@ -107,19 +129,5 @@ then
 
 else
     alias ls='ls -F --color=always '
-    #echo "No bashrc specific commands for this hosts."
+    echo "No bashrc specific commands for this hosts."
 fi
-# PROGRAM PATHS and VARIABLES
-# export PATH=$PATH:/opt/cuda/bin/
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda/lib/
-# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda/lib/
-
-#intel fortran
-#source /opt/intel/Compiler/11.1/069/bin/ifortvars.sh  ia32 
-
-# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda/lib/
-# LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/manu/Config/profiles/openGL/
-# export PATH=$PATH:/opt/cuda/bin/
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cuda/lib/
-
-
--- dotfiles/bashrc_user.bash
+++ dotfiles/bashrc_user.bash
@@ -1,10 +1,10 @@
+if [ ! -z ${BASHRC_LOADED} ]; then
+    echo "Bashrc was already loaded";
+fi
+
+source $HOME"/""Config/dotfiles/bashrc_machine.bash"
+source $HOME"/""Config/dotfiles/bashrc_common.bash"
+
+export BASHRC_LOADED=true
+
 # export PATH=$PATH:$HOME/Config/scripts/
-
-export HOME2=$HOME/
-source $HOME2"Config/dotfiles/bashrc_machine.bash"
-source $HOME2"Config/dotfiles/bashrc_common.bash"
-# # source $HOME2"Config/bash/git-completion.bash"
-
-
-#gpg --keyserver keyserver.ubuntu.com --recv 778978B00F7992B0
-#gpg --export --armor 778978B00F7992B0 | sudo apt-key add -
