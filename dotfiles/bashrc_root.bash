a99351363d841eb82df22c260e84ee0e87b4df3b
Emmanuel Branlard <manu@work.de-prod.dk>
2016-01-15 16:36:52 +0100

    _update dos2unix

--- dotfiles/bashrc_root.bash
+++ dotfiles/bashrc_root.bash
@@ -1,5 +1,6 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export HOMEMANU=/home/manu
+export HOMEMANU=/home/emmbr
 export PATH=$PATH:$HOMEMANU/Config/scripts/
 # source $HOME2"Config/dotfiles/bashrc_machine.bash"
 source $HOMEMANU/"Config/dotfiles/bashrc_common.bash"
@@ -8,4 +9,30 @@ source $HOMEMANU/"Config/dotfiles/bashrc_common.bash"
 alias ls='ls -F --color=always --group-directories-first'
 alias grep='grep --color=always'
 
-export PS1='\[[1m[31m\]\u\[[32m\]@\[[31m\]\h:\[[32m\]\w \[[31m\]\$ \[[32m\]\[[m\]'
+# --------------------------------------------------------------------------------
+# --- Colors 
+# --------------------------------------------------------------------------------
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+export PS1="$BRed\u$BGreen@$BRed\h:$BGreen\w $BYellow\$ $Color_Off"
