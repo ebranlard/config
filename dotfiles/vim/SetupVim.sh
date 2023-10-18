b8c038909be290a630e2ea46a247f91e102e27bd
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-10-18 14:38:35 -0600

    Update from Kestrel

--- dotfiles/vim/SetupVim.sh
+++ dotfiles/vim/SetupVim.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-mkdir -p tmp
 mkdir -p bundle
 cd bundle
 
 git clone https://github.com/kien/ctrlp.vim/
-# git clone https://github.com/kien/rainbow_parentheses.vim
 git clone https://github.com/vim-scripts/Align
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/tpope/vim-markdown.git
@@ -12,7 +10,7 @@ git clone https://github.com/vim-latex/vim-latex.git
 git clone https://github.com/d11wtq/ctrlp_bdelete.vim.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-dispath.git
+git clone https://github.com/tpope/vim-dispatch.git
 git clone https://github.com/vim-scripts/LargeFile
 # Airline and requirements
 git clone https://github.com/vim-airline/vim-airline-themes
@@ -21,15 +19,15 @@ git clone https://github.com/bling/vim-airline
 git clone https://github.com/tomtom/tlib_vim.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git 
 git clone https://github.com/garbas/vim-snipmate.git
-# git clone https://github.com/honza/vim-snippets.git # Snippets stored in my vim/snippet directory directly
 git clone https://github.com/salsifis/vim-transpose
 #
 git clone https://github.com/ebranlard/vim-matlab-behave
 git clone https://github.com/ebranlard/vim-simple-comment
 git clone https://github.com/ebranlard/vim-simple-compile
 git clone https://github.com/ebranlard/vim-tagbar-manu
-git clone https://github.com/ebranlard/vim-tagbar-manu
 
+# git clone https://github.com/kien/rainbow_parentheses.vim
+# git clone https://github.com/honza/vim-snippets.git # Snippets stored in my vim/snippet directory directly
 
 if [ $HOSTNAME == 'work' ]
 then
